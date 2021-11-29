35d4c78a57116ef1d239a8b4ee59f4cd93ce1c4c
Zongnan Bao <ppnk@pp14.local>
2021-11-28 23:15:06 -0800

    original channels, bn

--- deep_emotion.py
+++ deep_emotion.py
@@ -12,8 +12,9 @@ class Deep_Emotion(nn.Module):
         '''
         super(Deep_Emotion,self).__init__()
         self.conv1 = nn.Conv2d(1,10,3)      # 46x46x32
-        self.conv2 = nn.Conv2d(10,10,3)     # 44x44x64
         self.bn1 = nn.BatchNorm2d(10)     # 18x18x128
+        self.conv2 = nn.Conv2d(10,10,3)     # 44x44x64
+        self.bn2 = nn.BatchNorm2d(10)     # 18x18x128
         self.pool2 = nn.MaxPool2d(2,2)      # 22x22x64
 
         # replace conv3/4 with deformable convolution
@@ -21,8 +22,9 @@ class Deep_Emotion(nn.Module):
         self.de_conv4 = DeformableConv2d(in_channels=10, out_channels=10, kernel_size=3, stride=1, padding=0)
 
         self.conv3 = nn.Conv2d(10,10,3)    # 20x20x128
+        self.bn3 = nn.BatchNorm2d(10) 
         self.conv4 = nn.Conv2d(10,10,3)   # 18x18x128
-        self.bn2 = nn.BatchNorm2d(10)     # 18x18x128
+        self.bn4 = nn.BatchNorm2d(10)  
         self.pool4 = nn.MaxPool2d(2,2)
 
         # self.bn2 = nn.BatchNorm2d(128)     # 18x18x128
@@ -30,6 +32,7 @@ class Deep_Emotion(nn.Module):
         self.flatten = nn.Flatten()
         # self.fc1 = nn.Linear(9 * 9 * 128, 50)        # 
         self.fc1 = nn.Linear(810, 50)        # 
+        self.bn5 = nn.BatchNorm1d(50)
         self.fc2 = nn.Linear(50,7)
 
         # torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros', device=None, dtype=None)
