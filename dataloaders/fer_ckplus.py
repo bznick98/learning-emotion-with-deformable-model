68bbfe849279b3aa1f896fd3c83966cc0a3f13c5
Zongnan Bao <ppnk@pp14.local>
2021-11-29 17:13:42 -0800

    dataloaders for FER_CK+

---
+++ dataloaders/fer_ckplus.py
@@ -0,0 +1,65 @@
+from __future__ import print_function
+import os
+import pandas as pd
+import numpy as np
+from PIL import Image
+import torch
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from tqdm import tqdm
+
+
+class Plain_Dataset(Dataset):
+    def __init__(self, img_dir, transform=None):
+        '''
+        Pytorch Dataset class
+        params:-
+                 img_dir  : the directory of the images (root image dir)
+                 transform: pytorch transformation over the data
+        return :-
+                 image, labels
+        '''
+        self.label_names = {
+            "anger": 0,
+            "disgust": 1,
+            "fear": 2,
+            "happiness": 3,
+            "sadness": 4,
+            "surprise": 5,
+            "neutrality": 6
+        }
+        self.imgs = np.array([])
+        self.labels = np.array([])
+        # read all subdirectories
+        dirs = os.listdir(img_dir)
+        print(dirs)
+        for dir in dirs:
+            if dir not in self.label_names: continue
+            curr_label = self.label_names[dir]
+            for file in tqdm(os.listdir(img_dir + "/" + dir)):
+                img = Image.open(img_dir + "/" + dir + "/" + file)
+                self.imgs.append(img)
+                self.labels.append(curr_label)
+                
+        print("LOADED!")
+        print(self.imgs.shape)
+        print(len(self.labels))
+
+    def show(self):
+        pass
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        img = self.imgs[idx]
+        label = self.labels[idx]
+        if self.transform :
+            img = self.transform(img)
+        return img ,label
+
+
+if __name__ == "__main__":
+    test_dataset = Plain_Dataset("data/fer_ckplus_kdef")
+    test_loader = DataLoader(test_dataset,batch_size=128,shuffle = True,num_workers=0)
