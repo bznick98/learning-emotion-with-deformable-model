bea630f67bb1c5202f034232b1bb22c84d27e863
Zongnan Bao <nick19981122@163.com>
2021-12-06 01:44:16 -0800

    save/read h5 fer_ckplus

--- dataloaders/fer_ckplus_kdef.py
+++ dataloaders/fer_ckplus_kdef.py
@@ -8,10 +8,10 @@ import torchvision
 from torch.utils.data import Dataset, DataLoader, random_split
 from torchvision import transforms
 from tqdm import tqdm
-
+import h5py
 
 class FER_CKPLUS_Dataset(Dataset):
-    def __init__(self, img_dir, transform=None, is_train=True, **kargs):
+    def __init__(self, img_dir, transform=None, is_train=True, h5_path=None, **kargs):
         '''
         Pytorch Dataset class
         params:-
@@ -31,20 +31,42 @@ class FER_CKPLUS_Dataset(Dataset):
             "surprise": 5,
             "neutrality": 6
         }
-        self.imgs = []
-        self.labels = []
-        # read all subdirectories
-        dirs = os.listdir(img_dir)
-        for dir in dirs:
-            if dir not in self.label_names: continue
-            curr_label = self.label_names[dir]
-            for file in tqdm(os.listdir(img_dir + "/" + dir), desc=f"Loading {dir}"):
-                with Image.open(img_dir + "/" + dir + "/" + file) as img:
-                    self.imgs.append(np.array(img))
-                    self.labels.append(curr_label)
+        # either read from h5 file or read from image subdirectories
+        if h5_path and os.path.exists(h5_path):
+            print(f"Reading from h5 file: {h5_path}")
+            self.read_h5(filepath=h5_path)
+        else:
+            self.imgs = []
+            self.labels = []
+            # read all subdirectories
+            dirs = os.listdir(img_dir)
+            for dir in dirs:
+                if dir not in self.label_names: continue
+                curr_label = self.label_names[dir]
+                for file in tqdm(os.listdir(img_dir + "/" + dir), desc=f"Loading {dir}"):
+                    with Image.open(img_dir + "/" + dir + "/" + file) as img:
+                        self.imgs.append(np.array(img))
+                        self.labels.append(curr_label)
 
     def show(self):
         pass
+
+    def save_h5(self, save_dir="./data/", data_name="fer_ckplus"):
+        """
+        save train/val dataset as separate h5 file
+        """
+        with h5py.File(os.path.join(save_dir, data_name + ".h5"), 'w') as hf:
+            hf.create_dataset("imgs", data=self.imgs)
+            hf.create_dataset("labels", data=self.labels)
+
+    def read_h5(self, filepath="./data/fer_ckplus.h5"):
+        """
+        read h5 file into self.imgs and self.labels
+        """
+        with h5py.File(filepath, 'r') as hf:
+            self.imgs = torch.Tensor(hf['imgs'][:])
+            self.labels = torch.Tensor(hf['labels'][:])
+            print(type(self.imgs), type(self.labels))
 
     def __len__(self):
         return len(self.labels)
@@ -106,6 +128,9 @@ class FER_CKPLUS_Dataloader:
 
 
 
+
+
 if __name__ == "__main__":
-    test_dataset = FER_CKPLUS_Dataset("data/fer_ckplus_kdef")
-    test_loader = DataLoader(test_dataset,batch_size=128,shuffle = True,num_workers=0)
+    dataset = FER_CKPLUS_Dataset("data/fer_ckplus_kdef", h5_path="./data/fer_ckplus.h5")
+    # test_loader = DataLoader(test_dataset,batch_size=128,shuffle = True,num_workers=0)
+    # dataset.save_h5(save_dir="./data/", data_name="fer_ckplus")
