8af7c463a8f1e38257073e3987180e90d56b2491
Zongnan Bao <nick19981122@163.com>
2021-12-06 13:39:10 -0800

    train script (plot)

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ from models.vgg import Vgg
 # parsing args
 parser = argparse.ArgumentParser(description="Configuration of setup and training process")
 parser.add_argument('-s', '--setup', action='store_true', help='setup the dataset for the first time')
-parser.add_argument('-d', '--data', type=str, default='data/',
+parser.add_argument('-d', '--data', type=str, default='FER2013',
                             help='data folder that contains data files that downloaded from kaggle (icml_face_data.csv)')
 parser.add_argument('--model', type=str, default='de', help='DL model to run, can be one of {de, vgg}')
 parser.add_argument('-e', '--epochs', type=int, default=100, help= 'number of epochs')
@@ -29,7 +29,7 @@ parser.add_argument('-lr', '--learning_rate', type=float, default=0.0001, help='
 parser.add_argument('-bs', '--batch_size', type=int, default=128, help='training/validation batch size')
 parser.add_argument('-wd', '--weight_decay', type=float, default=1e-4, help='weight decay coeff(L2 regularization)')
 parser.add_argument('--show', action='store_true', help='Show 1 training sample.')
-args = parser.parse_args()  
+args = parser.parse_args([])  
 
 # cpu/gpu device setup
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -43,13 +43,20 @@ if device.type == 'cuda':
     print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB')
 
 
-def Train(epochs, dl, criterion, optmizer, device):
+def Train(epochs, data_loader, criterion, optmizer, device):
     '''
     Training Loop
     '''
     print("===================================Start Training===================================")
-    train_loader = dl.train_loader
-    val_loader = dl.val_loader
+    train_loader = data_loader.train_loader
+    val_loader = data_loader.val_loader
+    train_len = data_loader.train_len
+    val_len = data_loader.val_len
+
+    train_loss_arr = []
+    val_loss_arr = []
+    train_acc_arr = []
+    val_acc_arr = []
 
     for e in range(epochs):
         train_loss = 0
@@ -80,57 +87,19 @@ def Train(epochs, dl, criterion, optmizer, device):
             _, val_preds = torch.max(val_outputs,1)
             val_correct += torch.sum(val_preds == labels.data)
 
-        train_loss = train_loss/ dl.train_len
-        train_acc = train_correct.double() / dl.train_len
-        validation_loss =  validation_loss / dl.val_len
-        val_acc = val_correct.double() / dl.val_len
-
+        train_loss = train_loss/train_len
+        train_acc = train_correct.double() / train_len
+        validation_loss =  validation_loss / val_len
+        val_acc = val_correct.double() / val_len
         print('Epoch: {} \tTraining Loss: {:.8f} \tValidation Loss {:.8f} \tTraining Acuuarcy {:.3f}% \tValidation Acuuarcy {:.3f}%'
                                                            .format(e+1, train_loss,validation_loss,train_acc * 100, val_acc*100))
 
+        # save for plotting
+        train_loss_arr.append(train_loss)
+        train_acc_arr.append(train_acc)
+        val_loss_arr.append(val_loss)
+        val_acc_arr.append(val_acc)
+
     torch.save(net.state_dict(),'deep_emotion-{}-{}-{}.pt'.format(epochs,batchsize,lr))
     print("===================================Training Finished===================================")
-
-
-if __name__ == '__main__':    
-    # Choosing hyper-parameters  
-    epochs = args.epochs
-    lr = args.learning_rate
-    batchsize = args.batch_size
-    weight_decay = args.weight_decay
-
-    # Choosing model
-    if args.model == 'de':
-        net = Deep_Emotion(wider=False, deeper=False, de_conv=False)
-    elif args.model == 'vgg':
-        net = Vgg()
-    else:
-        raise Exception("--model can only accpet one of \{de, vgg\}")
-    net.to(device)
-
-    # Choosing dataset
-    if args.data == "FER2013": # FER2013
-        summary(net, input_size=(batchsize, 1, 48, 48))
-        dl = FER2013_Dataloader(gen_data=args.setup)
-        train_loader = dl.train_loader
-        val_loader = dl.val_loader
-    elif args.data == "FER_CKPLUS": # FER_CKPLUS
-        dl = FER_CKPLUS_Dataloader(resize=(48,48))
-        train_loader = dl.train_loader
-        val_loader = None
-        summary(net, input_size=(batchsize, 1, 48, 48))
-    elif args.data == "CK_PLUS":
-        dl = FER_CKPLUS_Dataloader(data_dir="data/CK_PLUS/")
-        train_loader = dl.train_loader
-        val_loader = None
-        summary(net, input_size=(batchsize, 1, 48, 48))
-    else:
-        raise Exception("-d or --data can only be \{FER2013, FER_CK\}")
-
-    # Loss
-    criterion = nn.CrossEntropyLoss()
-    # Optimizer
-    optmizer = optim.Adam(net.parameters(), lr=lr, weight_decay=weight_decay)
-    Train(epochs, train_loader, val_loader, criterion, optmizer, device)
-
-
+    return train_loss_arr, train_acc_arr, val_loss_arr, val_acc_arr
