8cd8211ba89f652cfccb6907917b5bc866c1afc0
Zongnan Bao <nick19981122@163.com>
2021-12-08 06:41:17 -0800

    deep-emotion224

--- models/deep_emotion.py
+++ models/deep_emotion.py
@@ -127,3 +127,126 @@ class Deep_Emotion(nn.Module):
 
         return out
 
+
+
+class Deep_Emotion224(nn.Module):
+    def __init__(self, wider=False, deeper=False, de_conv=False):
+        '''
+        Deep_Emotion (wider)
+        input: Nx1x224x224
+        '''
+        super(Deep_Emotion,self).__init__()
+        self.wider = wider
+        self.deeper = deeper
+        self.de_conv = de_conv
+
+        self.conv1 = nn.Conv2d(1,32,3)      # 222x222x32
+        self.bn1 = nn.BatchNorm2d(32)     
+        self.conv2 = nn.Conv2d(32,32,3)     # 220x220x32
+        self.bn2 = nn.BatchNorm2d(32)     
+        self.pool2 = nn.MaxPool2d(2,2)      # 110x110x32
+
+        # replace conv3/4 with deformable convolution
+        if de_conv:
+            self.de_conv3 = DeformableConv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=0)
+            self.de_conv4 = DeformableConv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=0)
+
+        self.conv3 = nn.Conv2d(32,64,3)    # 108x108x64
+        self.bn3 = nn.BatchNorm2d(64) 
+        self.conv4 = nn.Conv2d(64,64,3)    # 106x106x64
+        self.bn4 = nn.BatchNorm2d(64)  
+        self.conv5 = nn.Conv2d(64,64,3)    # 104x104x64
+        self.bn5 = nn.BatchNorm2d(64)  
+        self.pool5 = nn.MaxPool2d(2,2)     # 52x52x64
+
+        self.conv6 = nn.Conv2d(64,128,3)   # 50x50x128
+        self.bn6 = nn.BatchNorm2d(128) 
+        self.conv7 = nn.Conv2d(128,128,3)  # 48x48x128
+        self.bn7 = nn.BatchNorm2d(128)  
+        self.pool7 = nn.MaxPool2d(2,2)     # 46x46x128
+
+        self.conv8 = nn.Conv2d(128,16,1)   # 46x46x16
+        self.bn8 = nn.BatchNorm2d(16)  
+        self.pool8 = nn.MaxPool2d(2,2)     # 23x23x16
+
+        self.flatten = nn.Flatten()
+        # handle 48x48 input
+        self.fc1 = nn.Linear(23 * 23 * 16, 32)
+        self.bn_fc = nn.BatchNorm1d(32)
+        self.fc2 = nn.Linear(32,7)
+
+        # torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros', device=None, dtype=None)
+
+        # STN & localization network
+        self.localization = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=7),     # 42x42xch  /   # 218x218xch
+            nn.MaxPool2d(2, stride=2),          # 21x21xch  /   # 109x109xch
+            nn.ReLU(True),
+            nn.Conv2d(8, 10, kernel_size=5),    # 16x16xch  /   # 104x104xch
+            nn.MaxPool2d(2, stride=2),          # 8x8xch    /   # 52x52xch
+            nn.ReLU(True)
+        )
+
+        flat_len = 52 * 52 * 10 # 224x224 input for stn
+        self.fc_loc = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(flat_len, 32),
+            nn.ReLU(True),
+            nn.Linear(32, 3 * 2)
+        )
+        self.fc_loc[3].weight.data.zero_()
+        self.fc_loc[3].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+
+    def stn(self, x):
+        xs = self.localization(x)
+        # xs = xs.view(-1, 640)
+        theta = self.fc_loc(xs)
+        theta = theta.view(-1, 2, 3)
+
+        grid = F.affine_grid(theta, x.size())
+        x = F.grid_sample(x, grid)
+        return x
+
+    def forward(self,input):
+        out = self.stn(input)
+
+        out = F.relu(self.conv1(out))
+        out = self.bn1(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = F.relu(self.pool2(out))
+
+        # if using deformable convolution (only for 3/4 layer)
+        if self.de_conv:
+            out = F.relu(self.de_conv3(out))
+            out = self.bn3(out)
+            out = self.bn4(self.de_conv4(out))
+            out = F.relu(self.pool4(out))
+        else:
+            out = F.relu(self.conv3(out))
+            out = self.bn3(out)
+            out = self.bn4(self.conv4(out))
+            out = F.relu(self.pool4(out))
+
+        # deeper
+        out = F.relu(self.conv5(out))
+        out = self.bn5(out)
+        out = F.relu(self.conv6(out))
+        out = self.bn6(out)
+        out = F.relu(self.conv7(out))
+        out = self.bn7(out)
+        out = self.pool7(out)
+
+        out = F.relu(self.conv8(out))
+        out = self.bn8(out)
+        out = self.pool8(out)
+
+        out = F.dropout(out)
+        out = self.flatten(out)
+        out = F.relu(self.fc1(out))
+        out = self.bn_fc(out)
+        out = self.fc2(out)
+
+        return out
+
+
