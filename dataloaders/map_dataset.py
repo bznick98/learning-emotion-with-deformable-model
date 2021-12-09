39ce0f55967c7198ebb49ad60ecf9a5db401c2a6
Zongnan Bao <nick19981122@163.com>
2021-12-09 14:00:09 -0800

    map dataset

---
+++ dataloaders/map_dataset.py
@@ -0,0 +1,22 @@
+import torch
+from torch.utils.data import Dataset
+
+class MapDataset(torch.utils.data.Dataset):
+    """
+    Given a dataset, creates a dataset which applies a mapping function
+    to its items (lazily, only when an item is called).
+
+    Note that data is not cloned/copied from the initial dataset.
+    Source: https://discuss.pytorch.org/t/apply-different-transform-data-augmentation-to-train-and-validation/63580/2
+    """
+
+    def __init__(self, dataset, map_fn):
+        self.dataset = dataset
+        self.map = map_fn
+
+    def __getitem__(self, index):
+        return self.map(self.dataset[index])
+
+    def __len__(self):
+        return len(self.dataset)
+
